9f2dc1c7547a21e6009fe8f3ba4f205e2251b7e3
Nikita Romanoff <Nikromanoff2005@yandex.ru>
2026-03-24 13:37:30 +0300

    it just works

--- src/core.clj
+++ src/core.clj
@@ -17,24 +17,47 @@
 (defn get-fib-cache []
   @(-> fib meta :cache))
 
-(defn -main []
-  (println "мемоизация:")
-  
-  (let [n1 10
-        n2 15
-        n3 20
-        r1 (fib n1)
-        r2 (fib n2)
-        r3 (fib n3)]
-    
-    (println (format "Fib(%d) = %d" n1 r1))
-    (println (format "Fib(%d) = %d" n2 r2))
-    (println (format "Fib(%d) = %d" n3 r3))
-    
-    (println "\nкэш после вычислений:")
-    (let [cache (get-fib-cache)
-          sorted-keys (sort (keys cache))]
-      (doseq [k sorted-keys]
-        (println (format "  fib(%2d) = %d" k (get cache k))))))
-)
-(-main)
+(defn read-number []
+  (loop []
+    (println "Введите номер числа Фибоначчи: \n")
+    (flush)
+    (let [input (read-line)]
+      (if-let [num (try
+                     (parse-long input)
+                     (catch Exception _ nil))]
+        (if (>= num 0)
+          num
+          (do
+            (println "Ошибка! Число должно быть >= 0")
+            (recur)))
+        (do
+          (println "Ошибка! Введите целое число")
+          (recur))))))
+
+(defn main []
+  (println "\n=======================================")
+  (loop []
+    (println "1. Продолжить")
+    (println "2. Выход")
+    (println "Выбор: \n")
+    (flush)
+    (let [choice (read-line)]
+      (case choice
+        "1" (let [num (read-number)]
+              (if num
+                (let [result (time (fib num))]
+                  (println (format "\nFib(%d) = %d" num result)))
+                (let [cache (get-fib-cache)
+                      sorted-keys (sort (keys cache))]
+                  (doseq [n sorted-keys]
+                    (println (format "  fib(%2d) = %d" n (get cache n))))))
+              (println "\n--------------")
+              (recur))
+        "2" (println "\nВыход из программы!")
+        (do
+          (println "Неверный выбор. Попробуйте снова.")
+          (recur)))))
+  (println "----------------"))
+
+(comment
+  (main))
