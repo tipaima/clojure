fb6c396970f413e8ab98b98799139ea36ce1973a
Nikita Romanoff <Nikromanoff2005@yandex.ru>
2026-03-24 13:34:54 +0300

    please work

--- src/core.clj
+++ src/core.clj
@@ -14,9 +14,27 @@
              result))))
       {:cache cache})))
 
-(defn -main []
-  (println "Числа Фибоначчи от 0 до 20:")
-  (doseq [i (range 21)]
-    (println (format "Fib(%2d) = %d" i (fib i)))))
+(defn get-fib-cache []
+  @(-> fib meta :cache))
 
+(defn -main []
+  (println "мемоизация:")
+  
+  (let [n1 10
+        n2 15
+        n3 20
+        r1 (fib n1)
+        r2 (fib n2)
+        r3 (fib n3)]
+    
+    (println (format "Fib(%d) = %d" n1 r1))
+    (println (format "Fib(%d) = %d" n2 r2))
+    (println (format "Fib(%d) = %d" n3 r3))
+    
+    (println "\nкэш после вычислений:")
+    (let [cache (get-fib-cache)
+          sorted-keys (sort (keys cache))]
+      (doseq [k sorted-keys]
+        (println (format "  fib(%2d) = %d" k (get cache k))))))
+)
 (-main)
